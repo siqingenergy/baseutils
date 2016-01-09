54958677640076f856fd5448fc40c9b220765304
Siqing <fllqy_2006@163.com>
2016-01-09 22:57:47 +0800

    HomeActivity

---
+++ BaseUtils/app/src/main/java/com/siqing/maven/baseutils/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.siqing.maven.baseutils;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+/**
+ * Created by siqing on 16/1/9.
+ */
+public class HomeActivity extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
