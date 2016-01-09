c3d2d8b247f14748a37eba3d722497f7c535abcb
Siqing <fllqy_2006@163.com>
2016-01-09 23:19:38 +0800

    log

--- BaseUtils/app/src/main/java/com/siqing/maven/baseutils/HomeActivity.java
+++ BaseUtils/app/src/main/java/com/siqing/maven/baseutils/HomeActivity.java
@@ -3,6 +3,7 @@ package com.siqing.maven.baseutils;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.util.Log;
 
 /**
  * Created by siqing on 16/1/9.
@@ -11,7 +12,7 @@ public class HomeActivity extends BaseActivity {
 
     @Override
     protected void initViews() {
-
+        Log.e(getClass().getSimpleName(), "=======HomeActivity========");
     }
 
     @Override
